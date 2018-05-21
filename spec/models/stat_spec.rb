4e4be4f934d71c6b930287e1b024284ae23934e9
Aasshish Khullar <aasshish.khullar@goomo.com>
2018-05-21 19:14:12 +0530

    stats specs added and charset added in views

--- spec/models/stat_spec.rb
+++ spec/models/stat_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Stat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    let!(:stats) { build(:stat) }
+
+
+	  context "it validate the Link Object" do
+	    
+	    it "Link id should not be nil " do
+	     
+	     expect(stats.link_id).not_to be_nil
+	    end
+
+	    it "stat platform should not be nil " do
+	     expect(stats.platform).not_to be_nil
+	    end
+
+	    it "stat ip_address should not be nil " do
+	    expect(stats.ip_address).not_to be_nil
+	    end
+
+	  end
 end
